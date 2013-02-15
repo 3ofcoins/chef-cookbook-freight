532d006978c3ec262a5a4efe5bf1e5bbbb3e47cf
mbrennan-afa <mbrennan@afainsurance.com>
2013-02-15 15:22:19 +1100

    Rewritten to support greater configuration options.

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['freight']['lib_folder'] = "/var/lib/freight"
+default['freight']['cache_folder'] = "/var/cache/freight"
+default['freight']['origin'] = "Freight"
+default['freight']['label'] = "Freight"
